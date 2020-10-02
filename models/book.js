8e6a4acf34720e992c4d3c0135ae8741d1a5d631
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-02 20:43:33 +0800

    Moved book schema to models

---
+++ models/book.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const AuthorSchema = new Schema({
+  name: {
+    type: String,
+    trim: true
+  }
+});
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    trim: true
+  },
+  authors: [AuthorSchema],
+  description: String,
+  image: {
+    type: String,
+    trim: true
+  },
+  link: {
+    type: String,
+    trim: true
+  },
+  averageRating: Number
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
