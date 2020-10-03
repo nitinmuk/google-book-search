6577809d7f47b1e8fd5995e8dc0a4ad338a44ef9
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-03 11:39:46 +0800

    Added header component to display navigation menu

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    const [activeItem, setActiveItem] = useState();
+    const handleItemClick = (e, { name }) => setActiveItem({ activeItem: name });
+    return (
+        <Menu>
+            <Menu.Item header>Google Books</Menu.Item>
+            <Menu.Item
+                as={Link} to="/search"
+                name="Search"
+                active={activeItem === "Search"}
+                onClick={handleItemClick}
+            />
+            <Menu.Item
+                as={Link} to="/saved"
+                name="Saved"
+                active={activeItem === "Saved"}
+                onClick={handleItemClick}
+            />
+        </Menu>
+
+    );
+}
+
+export default Header;
