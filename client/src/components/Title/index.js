a734371b70c4846697e8567969f674bc53147541
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-03 15:58:42 +0800

    Added styling in title

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -3,8 +3,8 @@ import { Grid } from "semantic-ui-react";
 
 const Title = () => {
     return (
-        <Grid>
-            <Grid.Row style={{borderStyle: "solid", margin:"20px 20px 25px 20px"}}>
+        <Grid style={{ backgroundColor:"#2185d0", color: "white", margin: "20px 20px 25px 20px" }}>
+            <Grid.Row>
                 <Grid.Column>
                 <h2>Google Book Search</h2>
                 <p>Search And Save Books of Interest</p>
