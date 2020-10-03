47bacca11383f1c372a6faa6c1a7e53b42c4c7f9
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-03 16:01:26 +0800

    Updated Search Page to store search text and render search form

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
+import SearchForm from "../../components/SearchForm";
 const Search = () => {
+    const inputRef = useRef();
+    const handleSearchSubmit = () => {
+        console.log(inputRef.current.value);
+    }
     return (
         <div>
-            <Header/>
-            <Title/>
+            <Header />
+            <Title />
+            <SearchForm onClick={handleSearchSubmit} ref={inputRef}/>
         </div>
     );
 }
