46df6fe6cee82670c4f76535ac85fc23e8b3a159
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-03 11:41:09 +0800

    Created saved and search components

---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const Saved = () => {
+    return (
+        <div>
+            <Header/>
+            Saved
+        </div>
+
+    );
+}
+
+export default Saved;
---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header";
+const Search = () => {
+    return (
+        <div>
+            <Header/>
+            Search
+        </div>
+    );
+}
+
+export default Search;
