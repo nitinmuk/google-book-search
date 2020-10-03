66f18d3612dddc6c73b46c53ca8d5a24253e3897
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-03 11:41:52 +0800

    Added routing using react router for saved and search pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <Router>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Router>
     </div>
   );
 }
