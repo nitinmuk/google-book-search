3d104ccd9c38e220315305207fa6a458d405a110
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-04 09:35:40 +0800

    Created state to store search Result and added logic to trigger search and render same using SearchResults component

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,17 +1,30 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import SearchForm from "../../components/SearchForm";
+import API from "../../utils/API";
+import SearchResults from "../../components/SearchResults";
 const Search = () => {
     const inputRef = useRef();
-    const handleSearchSubmit = () => {
-        console.log(inputRef.current.value);
+    const [books, setBooks] = useState([]);
+    const [searchButtonState, setSearchButtonState] = useState(false);
+    const handleSearchSubmit = async () => {
+        setBooks([]);       
+        const searchText = inputRef.current.value.trim();
+        if (searchText !== "") {
+            setSearchButtonState(true);
+            const searchedBooks = await API.searchBooks(searchText);
+            setBooks(searchedBooks.data);
+            setSearchButtonState(false);
+            inputRef.current.value = "";
+        }
     }
     return (
         <div>
             <Header />
             <Title />
-            <SearchForm onClick={handleSearchSubmit} ref={inputRef}/>
+            <SearchForm onClick={handleSearchSubmit} ref={inputRef} disabled={searchButtonState} />
+            <SearchResults books={books}/>
         </div>
     );
 }
