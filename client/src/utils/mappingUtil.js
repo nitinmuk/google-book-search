3e382531d808ac19aca0a99bcc4043e013f1e1f1
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-04 14:27:04 +0800

    created file to store mapping logic and added a function to map google book into saved book

--- client/src/utils/mappingUtil.js
+++ client/src/utils/mappingUtil.js
@@ -1,13 +1,13 @@
 export default {
     mapGoogleBook: function(googleBook) {
+        
         return {
             title: googleBook.volumeInfo.title,
             subtitle: googleBook.volumeInfo.subtitle,
             authors: googleBook.volumeInfo.authors,
-            image: googleBook.volumeInfo.imageLinks.thumbnail,
+            image: googleBook.volumeInfo.imageLinks? googleBook.volumeInfo.imageLinks.thumbnail:undefined,
             description: googleBook.volumeInfo.description,
             link: googleBook.volumeInfo.infoLink,
-            averageRating: googleBook.volumeInfo.averageRating,
             id: googleBook.id
         }
     }
