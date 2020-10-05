5baf3ef785b2376e4e6e71bc2e1638a79d6018ef
Nitin Mukesh <nitinmukes@gmail.com>
2020-10-06 00:24:44 +0800

    Updated SearchForm to use Form component

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,29 +1,23 @@
 import React from "react";
-import { Grid, Button, Icon } from "semantic-ui-react";
+import { Button, Icon, Form } from 'semantic-ui-react'
 const SearchForm = React.forwardRef((props, ref) => {
     return (
-        <div>
-            <Grid style={{ backgroundColor: "#2185d0", color: "white", margin: "20px 20px 25px 20px" }}>
-                <Grid.Row >
-                    <Grid.Column as="h5" textAlign="left">
-                        Book Search
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>                    
-                <input type="text" ref={ref} label="Book" placeholder="type here..." style={{ margin: "0px 10px 0px 10px", padding: "10px 81% 10px 10px", border: "none"}}/>                    
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column floated="right" width={2}>
-                        <Button animated {...props}>
-                            <Button.Content visible>Search</Button.Content>
-                            <Button.Content hidden>
-                                <Icon name='search' />
-                            </Button.Content>
-                        </Button>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </div>
+        <Form style={{ backgroundColor: "#2185d0", color: "white", margin: "20px 20px 25px 20px", padding: "20px" }}>
+            <h5 style={{ textAlign:"left" }}> Search Book: </h5>
+            <Form.Group inline>                
+                <Form.Field width={12}>
+                    <input label="Book" placeholder='type here...' ref={ref} type="text"></input>
+                </Form.Field>
+                <Form.Field>
+                    <Button animated {...props} type="submit" floated="right">
+                        <Button.Content visible>Search</Button.Content>
+                        <Button.Content hidden>
+                            <Icon name='search' />
+                        </Button.Content>
+                    </Button>
+                </Form.Field>
+            </Form.Group>
+        </Form>
     );
 });
 
